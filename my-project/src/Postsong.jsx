4a3691b036e6419ebb934ee22975506319397905
FadliNur31 <133163475+FadliNur31@users.noreply.github.com>
2024-11-05 20:27:43 +0700

    Update Postsong.jsx

--- my-project/src/Postsong.jsx
+++ my-project/src/Postsong.jsx
@@ -74,7 +74,7 @@ const Postsong = ({ playlist }) => {
               client_id: client_id,
               scope: scope,
               redirect_uri: redirect_uri,
-              state: Math.random().toString(36).substring(2);,
+              state: Math.random().toString(36).substring(2),
             };
             window.location.href =
               "https://accounts.spotify.com/authorize?" + objectToQueryString(authParams);
